872bfbabc8a77c1a0cb198e94f9e44f4f63409c4
Guy Brukhis <guy.brukhis@klarna.com>
2016-07-31 14:30:45 +0300

    burst test

--- burst.sh
+++ burst.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
-echo "BURST!"
+day=`date +"%Y-%m-%d"`
+dir="/home/pi/burst"
+`mkdir ${dir}`
+
+
+while [ -f "/home/pi/lockfile" ]; do
+  sleep 1
+done
+
+`touch /home/pi/lockfile`
+
+counter=BURST_FRAME_RATE*BURST_DURATION_SEC
+
+while counter > 0
+do
+  timestamp=`date +"%Y-%m-%d-%H-%M-%S-%N"`
+  file="${dir}/${timestamp}.jpg"
+
+  shot="fswebcam -r 1080x1080 --no-banner ${file}"
+  echo "****** Running ${shot}"
+  eval $shot
+
+  # sleep 1/BURST_FRAME_RATE
+  let counter-=1
+done
+
+`rm /home/pi/lockfile`
+exit 0
