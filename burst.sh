262c265d42402f92464927d8d003bd43885d23a9
PI <pi@hackathon.com>
2016-07-31 18:43:11 +0300

    burst tweaks

--- burst.sh
+++ burst.sh
@@ -15,30 +15,24 @@ dir="/home/pi/burst"
 `mkdir ${dir}`
 
 while [ -f "/home/pi/lockfile" ]; do
+  echo 'Locked...'
   sleep 1
 done
 
-`touch /home/pi/lockfile`
+touch /home/pi/lockfile
 
 # Capturing images
-echo "Capturing images"
-`ffmpeg -t ${BURST_DURATION_SEC} -s 1280x960 -i /dev/video0 -r ${BURST_FRAME_RATE} -f image2 /home/pi/burst/image%04d.jpg`
+ffmpeg -t ${BURST_INPUT_DURATION_SEC} -s ${BURST_INPUT_RESOLUTION} -i /dev/video0 -r ${BURST_INPUT_FRAME_RATE} -f image2 /home/pi/burst/image%04d.jpg
 
 # Creating GIF
-echo "Creating GIF"
-`ffmpeg -f image2 -framerate 5 -pattern_type glob -i '/home/pi/burst/*.jpg' burst.gif`
-
+ffmpeg -f image2 -framerate ${BURST_OUTPUT_FRAME_RATE} -pattern_type glob -i '/home/pi/burst/*.jpg' -vf scale=${BURST_GIF_RESOLUTION} burst.gif
 # Creating MP4
-echo "Creating MP4"
-`ffmpeg -framerate 15 -pattern_type glob -i '/home/pi/burst/*.jpg' -c:v libx264 -vf crop=960:960:160:0 burst.mp4`
+ffmpeg -framerate ${BURST_OUTPUT_FRAME_RATE} -pattern_type glob -i '/home/pi/burst/*.jpg' -c:v libx264 -vf crop=960:960:160:0 burst.mp4
 
-quote=`shuf -n 1 quotes.txt`
 # Uploading to Instagram
-echo "Uploading to Instagram"
-video_path=`/home/pi/day.zip/instagram/instagram -u ${INSTAGRAM_USER} -p ${INSTAGRAM_PASS} -f burst.mp4 -c ""`
+video_path=`/home/pi/day.zip/instagram/instagram -u ${INSTAGRAM_USER} -p ${INSTAGRAM_PASS} -f burst.mp4`
 
 # Uploading to Slack
-echo "Uploading to Slack"
-`slack="curl -F file=@burst.gif -F channels=da_pi_team -F title='Minute.zip' -F initial_comment='Like it in instagram: ${video_path}. ${quote}' -F token=${SLACK_TOKEN} https://slack.com/api/files.upload | grep -o '\"ok\":true'"`
+curl -F file=@burst.gif -F channels=da_pi_team -F title='Minute.zip' -F initial_comment="Like it in Instagram: ${video_path}" -F token=${SLACK_TOKEN} https://slack.com/api/files.upload
 
 exit 0
