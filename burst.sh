f8eb125558bd19b86f16bdbc7756de10ae0ee715
Guy Brukhis <guy.brukhis@klarna.com>
2016-07-31 15:49:40 +0300

    burst - use full dir path

--- burst.sh
+++ burst.sh
@@ -10,10 +10,10 @@ while [ -f "/home/pi/lockfile" ]; do
 done
 
 `touch /home/pi/lockfile`
-`ffmpeg -t ${BURST_DURATION_SEC} -s 640x480 -i /dev/video0 -r ${BURST_FRAME_RATE} -f image2 ${dir}/image%04d.jpg`
+`ffmpeg -t ${BURST_DURATION_SEC} -s 640x480 -i /dev/video0 -r ${BURST_FRAME_RATE} -f image2 /home/pi/burst/image%04d.jpg`
 `rm /home/pi/lockfile`
 
-`ffmpeg -f image2 -framerate 5 -pattern_type glob -i '${dir}/*.jpg' -vf scale=500x500 out.gif`
-`ffmpeg -framerate 5 -pattern_type glob -i '${dir}/*.jpg' -c:v libx264 out.mp4`
+`ffmpeg -f image2 -framerate 5 -pattern_type glob -i '/home/pi/burst/*.jpg' -vf scale=500x500 out.gif`
+`ffmpeg -framerate 5 -pattern_type glob -i '/home/pi/burst/*.jpg' -c:v libx264 out.mp4`
 
 exit 0
