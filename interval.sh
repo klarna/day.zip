77e4bf0454a4e35ca58c708bbf736ca792197446
Juan Lulkin <maiz@lulk.in>
2016-07-31 13:10:39 +0300

    Add retry to interval

--- interval.sh
+++ interval.sh
@@ -4,16 +4,27 @@ day=`date +"%Y-%m-%d"`
 timestamp=`date +"%Y-%m-%d-%H-%M-%S"`
 dir="/home/pi/${day}"
 `mkdir ${dir}`
+file="${dir}/${timestamp}.jpg"
 
-# `touch /home/pi/"${day}/${timestamp}.txt"`
+shot="fswebcam -r 1080x1080 --no-banner ${file}"
+echo "****** Running ${shot}"
+eval $shot
 
-shot="fswebcam -r 1080x1080 --no-banner ${dir}/${timestamp}.jpg"
-
-if eval $shot ; then
+if [ -f "$file" ]
     echo "Pic taken successfully"
     exit 0
 else
-    echo "slack upload failed"
+    echo "slack upload failed, retrying..."
+    sleep 5
+    eval $shot
+
+    if [ -f "$file" ]
+        echo "Pic taken successfully in 2nd attempt"
+        exit 0
+    else
+        echo "yeap, doesnt work"
+        exit 1
+    fi
     exit 1
 fi
 
