580b838f6092b361eef62842ba0972e3913397d8
Juan Lulkin <maiz@lulk.in>
2016-07-31 15:51:34 +0300

    Improve interval

--- interval.sh
+++ interval.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/nash
+
+function finish {
+  rm /home/pi/lockfile
+}
+trap finish EXIT
 
 day=`date +"%Y-%m-%d"`
 timestamp=`date +"%Y-%m-%d-%H-%M-%S"`
 dir="/home/pi/daily/${day}"
 `mkdir ${dir}`
 file="${dir}/${timestamp}.jpg"
+
+while [ -f "/home/pi/lockfile" ]; do
+  echo "Locked..."
+  sleep 1
+done
+
+touch /home/pi/lockfile
 
 shot="fswebcam -r 1080x1080 --no-banner ${file}"
 echo "****** Running ${shot}"
